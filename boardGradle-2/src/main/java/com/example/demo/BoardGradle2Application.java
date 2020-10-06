8946e3597af9395277abfb5f9241ba9f5a904172
koje <koje@B202-21>
2020-10-06 11:29:48 +0900

    .

---
+++ boardGradle-2/src/main/java/com/example/demo/BoardGradle2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardGradle2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardGradle2Application.class, args);
+	}
+
+}
